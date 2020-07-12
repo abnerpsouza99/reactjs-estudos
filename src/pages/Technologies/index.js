c9fa3298d0bd7a612b865aa20c61399b2f426c24
abnerpsouza99 <abnertecnicosenac@gmail.com>
2020-07-12 15:10:32 -0300

    Inserts technologies component, and your styles

--- src/pages/Technologies/index.js
+++ src/pages/Technologies/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './style.css'
+import reactJSImage from '../../assets/react.png'
+import reactCSSImage from '../../assets/css.png'
+import reactHTMLImage from '../../assets/html.png'
+import jSImage from '../../assets/js.png'
 
 export default function Technologies() {
+
   return (
     <section className="technologiesSection">
+      <h2>Tecnologias</h2>
       <div className="frontEnd">
+        <h2>Front-End</h2>
         <ul className="frontEndList">
-          <li></li>
+          <li>
+            <img src={jSImage} alt="" />
+            <h3>JavaScript</h3>
+          </li>
+          <li>
+            <img src={reactHTMLImage} alt="" />
+            <h3>HTML</h3>
+          </li>
+          <li>
+            <img src={reactCSSImage} alt="" />
+            <h3>CSS</h3>
+          </li>
+          <li>
+            <img src={reactJSImage} alt="" />
+            <h3>ReactJS</h3>
+          </li>
         </ul>
       </div>
       <div className="backEnd">
