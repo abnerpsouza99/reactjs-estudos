cb9f0230789709fdd715c91943a15a2a8c18a3fa
Abner Pena de Souza <abner.souza@crm.gov.br>
2020-07-13 10:24:31 -0300

    Inserts the Animation with Scroll in Technologies Component

--- src/pages/Technologies/index.js
+++ src/pages/Technologies/index.js
@@ -6,46 +6,53 @@ import reactHTMLImage from '../../assets/html.png'
 import jSImage from '../../assets/js.png'
 import nodeImage from '../../assets/node.png'
 import sqlImage from '../../assets/sql.png'
+import ScrollAnimation from 'react-animate-on-scroll';
 
 export default function Technologies() {
 
   return (
     <section className="technologiesSection">
       <h2>Tecnologias</h2>
-      <div className="frontEnd">
-        <h2>Front-End</h2>
-        <ul className="frontEndList">
-          <li>
-            <img src={jSImage} alt="" />
-            <h3>JavaScript</h3>
-          </li>
-          <li>
-            <img src={reactHTMLImage} alt="" />
-            <h3>HTML</h3>
-          </li>
-          <li>
-            <img src={reactCSSImage} alt="" />
-            <h3>CSS</h3>
-          </li>
-          <li>
-            <img src={reactJSImage} alt="" />
-            <h3>ReactJS</h3>
-          </li>
-        </ul>
-      </div>
-      <div className="backEnd">
-      <h2>Back-End</h2>
-        <ul className="backEndList">
-          <li>
-          <img src={nodeImage} alt="" />
-            <h3>node.js</h3>
-          </li>
-          <li>
-          <img src={sqlImage} alt="" />
-            <h3>SQL</h3>
-          </li>
-        </ul>
-      </div>
+      <ScrollAnimation animateIn='bounceInRight' animateOut='bounceOutLeft'>
+        <div className="frontEnd">
+          <h2>Front-End</h2>
+          <ul className="frontEndList">
+            <li>
+              <img src={jSImage} alt="" />
+              <h3>JavaScript</h3>
+            </li>
+            <li>
+              <img src={reactHTMLImage} alt="" />
+              <h3>HTML</h3>
+            </li>
+            <li>
+              <img src={reactCSSImage} alt="" />
+              <h3>CSS</h3>
+            </li>
+            <li>
+              <img src={reactJSImage} alt="" />
+              <h3>ReactJS</h3>
+            </li>
+          </ul>
+        </div>
+
+      </ScrollAnimation>
+      <ScrollAnimation animateIn='bounceInLeft' animateOut='bounceOutRight'>
+        <div className="backEnd">
+          <h2>Back-End</h2>
+          <ul className="backEndList">
+            <li>
+              <img src={nodeImage} alt="" />
+              <h3>node.js</h3>
+            </li>
+            <li>
+              <img src={sqlImage} alt="" />
+              <h3>SQL</h3>
+            </li>
+          </ul>
+        </div>
+      </ScrollAnimation>
     </section>
+
   );
 }
