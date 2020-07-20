eaf219b09351726dc7982e30b707784c43082d38
abnerpsouza99 <abnertecnicosenac@gmail.com>
2020-07-20 18:55:53 -0300

    Modifies the style of social media icons

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,6 +4,7 @@ import instagramImg from '../../assets/instagramLogo.png'
 import githubImg from '../../assets/logoGithub.svg'
 import linkedinImg from '../../assets/linkedinLogo.png'
 import whatsAppImg from '../../assets/whatsappLogo.png'
+import {FaGithub, FaInstagram, FaLinkedin} from 'react-icons/fa'
 
 
 export default function Contact(){
@@ -14,9 +15,9 @@ export default function Contact(){
           <div className="socialMedias">
             <h3>Redes Sociais</h3>
             <ul className="socialMediaIcons">
-              <li><a href="https://www.instagram.com/abnerpsouza/"><img src={instagramImg}/></a></li>
-              <li><a href="https://github.com/abnerpsouza99"><img src={githubImg} alt=""/></a></li>
-              <li><a href="https://www.linkedin.com/in/abnerpenadesouza/"><img src={linkedinImg} alt=""/></a></li>
+              <li><a href="https://www.instagram.com/abnerpsouza/"><FaInstagram size="70px" className="instagramSvg"/></a></li>
+              <li><a href="https://github.com/abnerpsouza99"><FaGithub size="70px" className="githubSvg"/></a></li>
+              <li><a href="https://www.linkedin.com/in/abnerpenadesouza/"><FaLinkedin size="70px" className="linkedinSvg"/></a></li>
             </ul>
           </div>
           <div className="contacts">
